26a510fad65a0e6d6f6a0a85fc3eb5143fa1fd6d
pjordan34 <patrickjordan3483@gmail.com>
2018-12-11 13:00:22 -0500

    updates to files

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup,find_packages
 
 
 
-with open(‘requirements.txt’) as f:
+with open("requirements.txt") as f:
 	required = f.read().splitlines()
 
 setup(
-		name=‘nhl_analysis’,
-		author = ‘Patrick Jordan’,
-		description=‘package to conduct anlysis on NHL play by play data’,
-		license=‘MIT’
+		name="nhl_analysis",
+		author = "Patrick Jordan",
+		description="package to conduct anlysis on NHL play by play data",
+		license="MIT",
 		packages=find_packages('src'),
-		package_dir={'':'src'},
-		install_requires=required,
-		version=‘0.0’
-		url=‘https://github.com/pjordan34/nhl_analysis.git’
+		package_dir={'': 'src'},
+		url="https://github.com/pjordan34/nhl_analysis.git",
+		install_requires=list(required),
+		version="0.0"
 	)
