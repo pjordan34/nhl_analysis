ffc91928272ef890df50c72e43c9e98ee470ab12
pjordan34 <patrickjordan3483@gmail.com>
2018-12-11 12:15:08 -0500

    adjusted file structure

---
+++ src/nhl_analysis/event_filter.py
@@ -0,0 +1,177 @@
+"""
+Functions for subsetting/filtering the cleaned NHL Play by Play data. I leveraged hockey_scraper to get data from the NHL API.
+"""
+
+import pandas as pd
+import numpy as np
+import constants as const
+
+
+############### By team ######################################
+
+def by_home_team(pbp, team):
+    "Returns pbp from homes games for `team` (use the standard 3-letter abbreviation for team names.)"
+    return pbp[pbp['Home_Team'] == team]
+
+
+def by_away_team(pbp, team):
+    "Returns pbp from away games for `team` (use the standard 3-letter abbreviation for team names.)"
+    return pbp[pbp['Away_Team'] == team]
+
+
+def by_team(pbp, team):
+    "Returns pbp from all games for `team` (use the standard 3-letter abbreviation for team names.)"
+    home = by_home_team(pbp, team)
+    away = by_away_team(pbp, team)
+    return home.append(away).sort_values(by=['Game_Id'], axis=0)
+
+
+def by_event_team(pbp, team):
+    "Returns pbp where event team is `team` (use the standard 3-letter abbreviation for team names.)"
+    return pbp[pbp['Ev_Team'] == team]
+
+
+############### By players ######################################
+
+def by_goalie(pbp, goalie):
+    return pbp[(pbp['Home_Goalie'] == goalie) | (pbp['Away_Goalie'] == goalie)]
+
+
+def by_home_goalie(pbp, goalie):
+    return pbp[(pbp['Home_Goalie'] == goalie)]
+
+
+def by_away_goalie(pbp, goalie):
+    return pbp[(pbp['Away_Goalie'] == goalie)]
+
+
+def by_skater(pbp, player):
+    bool_vec = pd.Series(False, index=np.arange(len(pbp)))
+    for position in (const.HOME_SKATERS + const.AWAY_SKATERS):
+        bool_vec |= (pbp[position] == player)
+    return pbp[bool_vec]
+
+
+def by_home_skater(pbp, player):
+    bool_vec = pd.Series(False, index=np.arange(len(pbp)))
+    for position in const.HOME_PLAYERS:
+        bool_vec |= (pbp[position] == player)
+    return pbp[bool_vec]
+
+
+def by_away_skater(pbp, player):
+    bool_vec = pd.Series(False, index=np.arange(len(pbp)))
+    for position in const.AWAY_SKATERS:
+        bool_vec |= (pbp[position] == player)
+    return pbp[bool_vec]
+
+
+################### Period/Regulation/OT/Shootout ###########################
+
+def period(pbp, period):
+    "Return pbp from a given period of play."
+    return pbp[pbp['Period'] == period]
+
+
+def regulation(pbp):
+    "Return pbp from regulation time, i.e. periods 1,2,3."
+    return pbp[pbp['Period'] <= 3]
+
+
+def overtime(pbp):
+    "Return pbp from overtime, excluding shootouts."
+    after_regulation = pbp[pbp['Period'] > 3]
+    return remove_shootouts(after_regulation)
+
+
+def shootouts(pbp):
+    "Returns only shootout pbp."
+    # Shootouts are period 5 of regular season games.
+    return pbp[(pbp['Period'] == 5)
+                  & (pbp['Game_Id'] <= const.PLAYOFFS_START)]
+
+
+def remove_shootouts(pbp):
+    "Removes all shootout pbp."
+    # Shootouts are period 5 of regular season games.
+    return pbp[(pbp['Period'] != 5)
+                  | (pbp['Game_Id'] > const.PLAYOFFS_START)]
+
+
+################## Man-advantage status #######################
+
+def even_strength(pbp):
+    "Return even-strength (5v5, 4v4, 3v3) pbp. "
+    return pbp[pbp['Home_Players'] == pbp['Away_Players']]
+
+
+def five_on_five(pbp):  # goalies count!
+    return pbp[(pbp['Home_Players'] == 6) & (pbp['Away_Players'] == 6)]
+
+
+def four_on_four(pbp):  # goalies count!
+    return pbp[(pbp['Home_Players'] == 5) & (pbp['Away_Players'] == 5)]
+
+
+def man_advantage(pbp):
+    return pbp[pbp['Home_Players'] != pbp['Away_Players']]
+
+
+def power_play(pbp, team):
+    "Returns any power play pbp for specified team."
+    return pbp[((pbp['Home_Team'] == team)
+                   & (pbp['Home_Players'] > pbp['Away_Players']))
+                  | ((pbp['Away_Team'] == team)
+                     & (pbp['Away_Players'] > pbp['Home_Players']))]
+
+
+def penalty_kill(pbp, team):
+    "Returns any penalty_kill pbp for specified team."
+    return pbp[((pbp['Home_Team'] == team)
+                   & (pbp['Home_Players'] < pbp['Away_Players']))
+                  | ((pbp['Away_Team'] == team)
+                     & (pbp['Away_Players'] < pbp['Home_Players']))]
+
+
+###################### Season status ##############################
+
+def regular_season(pbp):
+    "Return pbp from regular season games."
+    return pbp[pbp['Game_Id'] <= const.PLAYOFFS_START]
+
+
+def playoffs(pbp):
+    "Return pbp from playoff games."
+    return pbp[pbp['Game_Id'] > const.PLAYOFFS_START]
+
+
+#################### Offensive events ##############################
+
+def goals(pbp):
+    return pbp[pbp['Event'] == 'GOAL']
+
+def hits(pbp):
+    return pbp[pbp['Event'] == 'HIT']
+
+
+def shots(pbp):
+    return pbp[(pbp['Event'] == 'SHOT') | (pbp['Event'] == 'GOAL')]
+
+
+def shot_attempts(pbp):
+    return pbp[(pbp['Event'] == 'SHOT') | (pbp['Event'] == 'GOAL')
+                  | (pbp['Event'] == 'MISS') | (pbp['Event'] == 'BLOCK')]
+
+
+
+#################### Defensive events ##############################
+
+def blocked_shots(pbp):
+    return pbp[pbp['Event'] == 'BLOCK']
+
+
+#################### Goalie events ###################################
+
+def saves(pbp):
+
+    return pbp[pbp['Event'] == 'SHOT']
---
+++ src/nhl_analysis/transform.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+
+
+# Read in the csv after it has run through the clean script in a loop
+def transform(pbp):
+    """" feed transform a play by play dataframe, I would keep it to one season at a time, prior to putting the dataframe 
+    in you can filter the dataframe by regular season/playoffs and eliminate shootouts if you want. this returns a new 
+    dataframe with aggregated stats by game by team.
+    """""
+    # drop columns that are not needed for team aggrigations
+    pbp.drop(['Description', 'Time_Elapsed',
+                'Seconds_Elapsed', 'Strength', 'Ev_Zone', 'Type',
+                'Home_Zone', 'p1_name', 'p1_ID', 'p2_name',
+                'p2_ID', 'p3_name', 'p3_ID', 'awayPlayer1', 'awayPlayer1_id',
+                'awayPlayer2', 'awayPlayer2_id', 'awayPlayer3', 'awayPlayer3_id',
+                'awayPlayer4', 'awayPlayer4_id', 'awayPlayer5', 'awayPlayer5_id',
+                'awayPlayer6', 'awayPlayer6_id', 'homePlayer1', 'homePlayer1_id',
+                'homePlayer2', 'homePlayer2_id', 'homePlayer3', 'homePlayer3_id',
+                'homePlayer4', 'homePlayer4_id', 'homePlayer5', 'homePlayer5_id',
+                'homePlayer6', 'homePlayer6_id', 'Away_Players', 'Home_Players', 'Away_Goalie', 'Away_Goalie_Id',
+                'Home_Goalie',
+                'Home_Goalie_Id', 'xC', 'yC', 'Home_Coach', 'Away_Coach'], axis=1, inplace=True)
+    # create filters to reduce the amount of rows you have to compute on.
+    pbpShotAtt = pbp[pbp.Event.isin(['SHOT', 'GOAL', 'MISS', 'BLOCK'])]
+    pbpMiss = pbp[pbp.Event.isin(['MISS'])]
+    pbpSOG = pbp[pbp.Event.isin(['SHOT', 'GOAL'])]
+    # This one will create a off row df from the others, so we will merge this back in last
+    pbpGoal = pbp[pbp.Event.isin(['GOAL'])]
+    pbpHit = pbp[pbp.Event.isin(['HIT'])]
+    pbpBlock = pbp[pbp.Event.isin(['BLOCK'])]
+    # set a MultiIndex to start your filtering
+    pbp.set_index(['Game_Id', 'Ev_Team'], inplace=True)
+    # Aggregations by game, and a concatonate at the end
+    pbpShotAtt = (pbpShotAtt.groupby(['Game_Id', 'Ev_Team'])['Event'].size().reset_index())
+    pbpShotAtt.rename(columns={'Event': 'Shot_Att'}, inplace=True)
+    pbpSOG = (pbpSOG.groupby(['Game_Id', 'Ev_Team'])['Event'].size().reset_index())
+    pbpSOG.rename(columns={'Event': 'SOG_for'}, inplace=True)
+    pbpHit = (pbpHit.groupby(['Game_Id', 'Ev_Team'])['Event'].size().reset_index())
+    pbpHit.rename(columns={'Event': 'Hits_for'}, inplace=True)
+    pbpGoal = (pbpGoal.groupby(['Game_Id', 'Ev_Team'])['Event'].size().reset_index())
+    pbpGoal.rename(columns={'Event': 'Goals_for'}, inplace=True)
+    pbpblocks = (pbpBlock.groupby(['Game_Id', 'Ev_Team'])['Event'].size().reset_index())
+    pbpblocks.rename(columns={'Event': 'Blocks_A'}, inplace=True)
+    pbpMiss = (pbpMiss.groupby(['Game_Id', 'Ev_Team'])['Event'].size().reset_index())
+    pbpMiss.rename(columns={'Event': 'Misses'}, inplace=True)
+    pbpO = pd.concat(
+        [pbpShotAtt, pbpMiss['Misses'], pbpSOG['SOG_for'], pbpHit['Hits_for'], pbpblocks['Blocks_A']], axis=1)
+    # at this point we need the 'Against' metrics which are really the opposite of the calculations we have already made, so we make a loop to do that.
+    # this is very repetiative and there may be a better way about it, but this is what I came up with
+    games_list = list(pbpO.Game_Id.unique())
+
+    pbpO['Blocks_for'] = 0
+    j = 1
+    for i in range(len(games_list)):
+        pbpO['Blocks_for'][i * 2] = pbpO['Blocks_A'][i + j]
+        pbpO['Blocks_for'][i + j] = pbpO['Blocks_A'][i * 2]
+        j = j + 1
+
+    pbpO['Shot_Att_A'] = 0
+    j = 1
+    for i in range(len(games_list)):
+        pbpO['Shot_Att_A'][i * 2] = pbpO['Shot_Att'][i + j]
+        pbpO['Shot_Att_A'][i + j] = pbpO['Shot_Att'][i * 2]
+        j = j + 1
+
+    pbpO['SOG_A'] = 0
+    j = 1
+    for i in range(len(games_list)):
+        pbpO['SOG_A'][i * 2] = pbpO['SOG_for'][i + j]
+        pbpO['SOG_A'][i + j] = pbpO['SOG_for'][i * 2]
+        j = j + 1
+
+    pbpO['Hits_A'] = 0
+    j = 1
+    for i in range(len(games_list)):
+        pbpO['Hits_A'][i * 2] = pbpO['Hits_for'][i + j]
+        pbpO['Hits_A'][i + j] = pbpO['Hits_for'][i * 2]
+        j = j + 1
+
+    # Now merge in the Goals_For column
+    pbpcomplete = pbpO.merge(right=pbpGoal, left_on=['Game_Id', 'Ev_Team'], right_on=['Game_Id', 'Ev_Team'],
+                                 how='left')
+    # and fill in the NaN values with 0
+    pbpcomplete.fillna(0, inplace=True)
+
+    # one more loop to bring in Goals_A
+    pbpcomplete['Goals_A'] = 0
+    j = 1
+    for i in range(len(games_list)):
+        pbpcomplete['Goals_A'][i * 2] = pbpcomplete['Goals_for'][i + j]
+        pbpcomplete['Goals_A'][i + j] = pbpcomplete['Goals_for'][i * 2]
+        j = j + 1
+
+    # now we can calculate Saves
+
+    pbpcomplete['Saves'] = pbpcomplete['SOG_A'] - pbpcomplete['Goals_A']
+    # add in stats and fancy stats
+    pbpcomplete['Shot_Percentage_for'] = round((pbpcomplete['Goals_for'] / pbpcomplete['SOG_for']) * 100, 2)
+    pbpcomplete['Fenwick_for'] = pbpcomplete['SOG_for'] + pbpcomplete['Misses']
+    pbpcomplete['Corsi_for'] = pbpcomplete['Shot_Att']
+    pbpcomplete['Corsi_A'] = pbpcomplete['Shot_Att_A']
+    pbpcomplete['Fenwick_A'] = pbpcomplete['Shot_Att_A'] - pbpcomplete['Blocks_for']
+    pbpcomplete['FSH%'] = round((pbpcomplete['Goals_for'] / pbpcomplete['Fenwick_for']) * 100, 2)
+    pbpcomplete['Miss%'] = round((pbpcomplete['Misses'] / pbpcomplete['Fenwick_for']) * 100, 2)
+    pbpcomplete['wshF'] = round(
+        (pbpcomplete['Goals_for'] + (0.2 * (pbpcomplete['Corsi_for'] - pbpcomplete['Goals_for']))), 2)
+    pbpcomplete['wshA'] = round(
+        (pbpcomplete['Goals_A'] + (0.2 * (pbpcomplete['Corsi_A'] - pbpcomplete['Goals_A']))), 2)
+    pbpcomplete['Goals_for%'] = round(
+        (pbpcomplete['Goals_for'] / (pbpcomplete['Goals_for'] + pbpcomplete['Goals_A'])) * 100, 2)
+    pbpcomplete['Fenwick_for%'] = round(
+        (pbpcomplete['Fenwick_for'] / (pbpcomplete['Fenwick_for'] + pbpcomplete['Fenwick_A'])) * 100, 2)
+    pbpcomplete['Corsi_for%'] = round(
+        (pbpcomplete['Corsi_for'] / (pbpcomplete['Corsi_for'] + pbpcomplete['Corsi_A'])) * 100, 2)
+    pbpcomplete['wshF%'] = round((pbpcomplete['wshF'] / (pbpcomplete['wshF'] + pbpcomplete['wshA'])) * 100, 2)
+    pbpcomplete['Sv%'] = round((pbpcomplete['Saves'] / pbpcomplete['SOG_A']) * 100, 2)
+
+    # and finally to calculate wins. I am going to count ties as a win for now since both teams get a standings point. you can have better control on this if you include shootouts.
+    pbpcomplete['Win'] = np.where(np.logical_or(pbpcomplete['Goals_for'] > pbpcomplete['Goals_A'],
+                                                  pbpcomplete['Goals_for'] == pbpcomplete['Goals_A']), 1, 0)
+    return pbpcomplete
